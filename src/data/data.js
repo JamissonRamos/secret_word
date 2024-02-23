c65753569394f2a9a8569dde1435fe77e082839a
Jamisson Lira Ramos <missonlira@gmail.com>
2024-02-23 09:34:33 -0300

    data => data.js criando arquivo de dados das palavras

---
+++ src/data/data.js
@@ -0,0 +1,279 @@
+export const wordList = {
+    carro: [
+      "Motor", "Porta", "Capô", "Pneu", "Antena",
+      "Freio", "Farol", "Vidro", "Cinto", "Óleo",
+      "Ignição", "Retrovisor", "Banco", "Roda", "Transmissão",
+      "Suspensão", "Escape", "Embreagem", "Tanque", "Radiador"
+    ],
+    fruta: [
+      "Banana", "Maçã", "Pêra", "Mamão", "Laranja",
+      "Uva", "Morango", "Abacaxi", "Kiwi", "Melancia",
+      "Manga", "Pêssego", "Cereja", "Goiaba", "Abacate",
+      "Caju", "Limão", "Ameixa", "Pitanga", "Maracujá"
+    ],
+    corpo: [
+      "Braço", "Perna", "Cérebro", "Pescoço", "Olhos",
+      "Orelha", "Dedo", "Unha", "Cabelo", "Dente",
+      "Língua", "Barriga", "Costas", "Joelho", "Cotovelo",
+      "Cintura", "Canela", "Tornozelo", "Sobrancelha", "Cílios"
+    ],
+    computador: [
+      "Mouse", "Teclado", "Monitor", "Gabinete",
+      "Placa-mãe", "Processador", "Memória", "Armazenamento", "Gráficos",
+      "Porta USB", "Sistema Operacional", "Rede", "Wi-Fi", "Bluetooth",
+      "Mousepad", "Webcam", "Caixas de Som", "Microfone", "Energia"
+    ],
+    programação: [
+      "Linguagem", "Framework", "JavaScript", "React",
+      "Node.js", "Python", "Java", "HTML", "CSS",
+      "API", "Biblioteca", "Debugging", "Versionamento", "Git",
+      "Terminal", "Banco de Dados", "Express", "Vue.js", "Angular"
+    ],
+    alimento: [
+      "Arroz", "Feijão", "Carne", "Leite", "Ovo",
+      "Macarrão", "Pão", "Queijo", "Frango", "Peixe",
+      "Salada", "Batata", "Tomate", "Cenoura", "Frutas",
+      "Iogurte", "Sopa", "Chocolate", "Cereal", "Sorvete"
+    ],
+    casa: [
+      "Porta", "Janela", "Telhado", "Sala", "Cozinha",
+      "Quarto", "Banheiro", "Corredor", "Escada", "Sótão",
+      "Porão", "Varanda", "Pátio", "Garagem", "Cortinas",
+      "Tapete", "Luminária", "Mesa", "Cadeira", "Sofá"
+    ],
+    animal: [
+      "Cachorro", "Gato", "Elefante", "Leão", "Pássaro",
+      "Tigre", "Urso", "Girafa", "Zebra", "Cobra",
+      "Macaco", "Rato", "Cavalo", "Coelho", "Peixe",
+      "Borboleta", "Abelha", "Galinha", "Pinguim", "Polvo"
+    ],
+    esporte: [
+      "Futebol", "Basquete", "Natação", "Ciclismo", "Vôlei",
+      "Tênis", "Golfe", "Handebol", "Surfe", "Atletismo",
+      "Boxe", "Esqui", "Mergulho", "Hóquei", "Rugby",
+      "Ginástica", "Automobilismo", "Arco e Flecha", "Badminton", "Skate"
+    ],
+    música: [
+      "Guitarra", "Piano", "Bateria", "Violino", "Flauta",
+      "Saxofone", "Trompete", "Baixo", "Canto", "Composição",
+      "Orquestra", "Rock", "Pop", "Jazz", "Blues",
+      "Clássica", "Rap", "Reggae", "Country", "Eletrônica"
+    ],
+    filme: [
+      "Ação", "Comédia", "Drama", "Ficção Científica", "Animação",
+      "Suspense", "Romance", "Terror", "Fantasia", "Documentário",
+      "Super-herói", "Aventura", "Musical", "Crime", "Mistério",
+      "Histórico", "Western", "Guerra", "Biografia", "Infantil"
+    ],
+    pais: [
+      "Brasil", "Estados Unidos", "China", "Índia", "Rússia",
+      "Alemanha", "Austrália", "Canadá", "Espanha", "México",
+      "França", "Itália", "Japão", "Reino Unido", "Argentina",
+      "África do Sul", "Turquia", "Coreia do Sul", "Indonésia", "Egito"
+    ],
+    cidade: [
+      "Nova York", "Tóquio", "Paris", "Londres", "Roma",
+      "Berlim", "Sydney", "Rio de Janeiro", "Pequim", "Cidade do México",
+      "Bangkok", "Istambul", "Toronto", "Cairo", "Mumbai",
+      "Cidade do Cabo", "Los Angeles", "Moscou", "Amsterdã", "Singapura"
+    ],
+    instrumento: [
+      "Trompete", "Saxofone", "Violoncelo", "Tambor", "Harpa",
+      "Oboé", "Gaita", "Clarinet", "Tuba", "Violão",
+      "Flauta de Pan", "Cítara", "Banjo", "Marimba", "Theremin",
+      "Xilofone", "Fagote", "Cavaquinho", "Didgeridoo", "Esraj"
+    ],
+    profissao: [
+      "Médico", "Engenheiro", "Professor", "Artista", "Advogado",
+      "Cientista", "Programador", "Designer", "Jornalista", "Policial",
+      "Bombeiro", "Piloto", "Chef", "Psicólogo", "Ator",
+      "Escritor", "Arquiteto", "Enfermeiro", "Fotógrafo", "Empresário"
+    ],
+    hobby: [
+      "Pintura", "Leitura", "Caminhada", "Pesca", "Fotografia",
+      "Viagem", "Culinária", "Ciclismo", "Dança", "Teatro",
+      "Esportes Radicais", "Jardinagem", "Música", "Colecionar", "Artesanato",
+      "Desenho", "Acampamento", "Surfe", "Modelagem", "Costura"
+    ],
+    cor: [
+      "Azul", "Verde", "Vermelho", "Amarelo", "Roxo",
+      "Laranja", "Rosa", "Marrom", "Preto", "Branco",
+      "Cinza", "Dourado", "Prateado", "Turquesa", "Bege",
+      "Verde-água", "Magenta", "Violeta", "Ciano", "Ametista"
+    ],
+    país: [
+      "Alemanha", "Austrália", "Canadá", "Espanha", "México",
+      "Itália", "Índia", "Rússia", "China", "Japão",
+      "França", "Brasil", "Estados Unidos", "Reino Unido", "Argentina",
+      "Portugal", "Suíça", "Holanda", "Chile", "África do Sul"
+    ],
+    montanha: [
+      "Everest", "K2", "Matterhorn", "Aconcágua", "Kangchenjunga",
+      "Denali", "Monte Elbrus", "Annapurna", "Monte McKinley", "Monte Vinson",
+      "Monte Kilimanjaro", "Monte Logan", "Monte Hood", "Monte Fuji", "Monte Cook",
+      "Monte Blanc", "Monte Rosa", "Monte McKinley", "Monte Kenya", "Monte Olympus"
+    ],
+    jogo: [
+      "Xadrez", "Poker", "Monopólio", "Catan", "Damas",
+      "Dominó", "Jogo da Vida", "RPG", "Quebra-cabeça", "Banco Imobiliário",
+      "Truco", "Buraco", "War", "Xadrez Chinês", "Batalha Naval",
+      "Uno", "Gamão", "Palavras Cruzadas", "Sudoku", "Ludo"
+    ],
+    transporte: [
+      "Avião", "Barco", "Bicicleta", "Ônibus", "Caminhão",
+      "Carro Elétrico", "Metrô", "Trem", "Motocicleta", "Carroça",
+      "Carruagem", "Helicóptero", "Bonde", "Teleférico", "Navio de Cruzeiro",
+      "Patins", "Skate", "Cavalo", "Carro de Corrida", "Submarino"
+    ],
+    bebida: [
+      "Café", "Chá", "Suco", "Refrigerante", "Água",
+      "Leite", "Cerveja", "Vinho", "Coquetel", "Champagne",
+      "Whisky", "Vodka", "Rum", "Tequila", "Sake",
+      "Smoothie", "Margarita", "Mojito", "Cachaça", "Gin"
+    ],
+    livro: [
+      "Dom Quixote", "Cem Anos de Solidão", "1984", "Orgulho e Preconceito", "A Bíblia",
+      "O Senhor dos Anéis", "Harry Potter", "Ulisses", "Crime e Castigo", "O Pequeno Príncipe",
+      "Ensaio sobre a Cegueira", "Matar um Rouxinol", "O Sol é para Todos", "O Grande Gatsby", "Odisseia",
+      "A Ilíada", "A Divina Comédia", "Drácula", "Os Miseráveis", "Em Busca do Tempo Perdido"
+    ],
+    tecnologia: [
+      "Inteligência Artificial", "Blockchain", "Realidade Virtual", "5G", "IoT",
+      "Machine Learning", "Big Data", "Criptomoeda", "Robótica", "Impressão 3D",
+      "Nanotecnologia", "Biotecnologia", "Realidade Aumentada", "Computação Quântica", "Cibersegurança",
+      "Cloud Computing", "Internet das Coisas", "Desenvolvimento Ágil", "Redes Neurais", "Deep Learning"
+    ],
+    planta: [
+      "Rosa", "Orquídea", "Girassol", "Cacto", "Samambaia",
+      "Lírio", "Bromélia", "Hortênsia", "Jasmim", "Lavanda",
+      "Manjericão", "Alecrim", "Gengibre", "Margarida", "Suculenta",
+      "Cerejeira", "Dália", "Papoula", "Crisântemo", "Gérbera"
+    ],
+    planeta: [
+      "Marte", "Vênus", "Júpiter", "Saturno", "Netuno",
+      "Urano", "Mercúrio", "Plutão", "Terra", "Kepler-186f",
+      "Gliese 581g", "HD 209458 b", "TrES-2b", "CoRoT-7b", "WASP-121b",
+      "55 Cancri e", "K2-18b", "TOI 700 d", "Proxima Centauri b", "Tau Ceti e"
+    ],
+    idioma: [
+      "Inglês", "Espanhol", "Mandarim", "Francês", "Alemão",
+      "Italiano", "Português", "Russo", "Japonês", "Árabe",
+      "Hindi", "Bengali", "Punjabi", "Turco", "Coreano",
+      "Holandês", "Grego", "Hebraico", "Sueco", "Norueguês"
+    ],
+    moda: [
+      "Vestido", "Camiseta", "Calça", "Sapato", "Gravata",
+      "Casaco", "Saia", "Bermuda", "Blusa", "Cachecol",
+      "Chapéu", "Óculos de Sol", "Relógio", "Bolsa", "Tênis",
+      "Anel", "Brinco", "Colar", "Luvas", "Jaqueta"
+    ],
+    clima: [
+      "Ensolarado", "Chuvoso", "Neve", "Vento", "Tempestade",
+      "Nublado", "Frio", "Quente", "Fresco", "Tornado",
+      "Chuva de Granizo", "Aurora Boreal", "Tufão", "Ciclone", "Névoa",
+      "Arco-íris", "Orvalho", "Geada", "Granizo", "Harmattan"
+    ],
+    férias: [
+      "Praia", "Montanha", "Cidade", "Camping", "Cruzeiro",
+      "Resort", "Parque Nacional", "Safari", "Esqui", "Caminhada",
+      "Mergulho", "Aventura", "Histórico", "Relaxamento", "Cultural",
+      "Ecoturismo", "Lua de Mel", "Viagem de Trem", "Roteiro Gastronômico", "Turismo de Aventura"
+    ],
+    objeto: [
+      "Relógio", "Óculos", "Câmera", "Celular", "Chave",
+      "Laptop", "Livro", "Caneta", "Notebook", "Garfo",
+      "Guarda-chuva", "Bolsa", "Agenda", "Fone de Ouvido", "Lanterna",
+      "Mochila", "Squeeze", "Bicicleta", "Chapéu", "Cinto"
+    ],
+    fenômeno: [
+      "Eclipse", "Tornado", "Terremoto", "Arco-íris", "Aurora Boreal",
+      "Cometa", "Vulcão", "Meteoro", "Ciclone", "Tsunami",
+      "Halo Solar", "Halo Lunar", "Neblina", "Miragem", "Espectro Solar",
+      "Fumaça de Incêndio", "Higrômetro", "Deslizamento de Terra", "Gravidade Zero", "Maré Vermelha"
+    ],
+    arquitetura: [
+      "Gótico", "Barroco", "Renascentista", "Contemporâneo", "Futurista",
+      "Art Déco", "Neoclássico", "Românico", "Rococó", "Modernista",
+      "Pós-moderno", "Brutalista", "Bizantino", "Colonial", "Vitoriano",
+      "Eclético", "High-Tech", "Arte Nova", "Arquitetura Verde", "Minimalista"
+    ],
+    herói: [
+      "Superman", "Spider-Man", "Wonder Woman", "Iron Man", "Batman",
+      "Captain America", "Black Widow", "Thor", "Hulk", "Aquaman",
+      "Flash", "Green Lantern", "Black Panther", "Doctor Strange", "Ant-Man",
+      "Wonder Woman", "Superwoman", "Supergirl", "Shazam", "Cyborg"
+    ],
+    vilão: [
+      "Joker", "Darth Vader", "Thanos", "Coringa", "Cruella",
+      "Loki", "Lex Luthor", "Green Goblin", "Catwoman", "Magneto",
+      "Jigsaw", "Harley Quinn", "Venom", "Ra's al Ghul", "The Penguin",
+      "The Riddler", "Two-Face", "Poison Ivy", "Ursula", "Maleficent"
+    ],
+    sobremesa: [
+      "Bolo", "Sorvete", "Torta", "Chocolate", "Pudim",
+      "Cupcake", "Brownie", "Cheesecake", "Mousse", "Gelatina",
+      "Creme Brûlée", "Tiramisu", "Macarons", "Pastel de Nata", "Profiteroles",
+      "Cannoli", "Parfait", "Strudel", "Donuts", "Sundae"
+    ],
+    oceano: [
+      "Atlântico", "Pacífico", "Índico", "Ártico", "Antártico",
+      "Mar do Caribe", "Mar Mediterrâneo", "Mar da China Meridional", "Mar de Bering", "Mar de Java",
+      "Golfo do México", "Mar Vermelho", "Mar de Coral", "Mar da Noruega", "Mar de Okhotsk",
+      "Mar de Weddell", "Mar da Irlanda", "Mar da Tasmanha", "Mar de Ross", "Mar de Labrador"
+    ],
+    bebê: [
+      "Chupeta", "Fralda", "Mamadeira", "Berço", "Brinquedo",
+      "Carrinho de Bebê", "Cadeirinha de Alimentação", "Babador", "Móbile", "Cadeirinha para Carro",
+      "Andador", "Cadeirão", "Chocalho", "Trocador", "Termômetro",
+      "Cama Infantil", "Tapete de Atividades", "Cadeira de Balanço", "Almofada de Amamentação", "Carrinho de Passeio"
+    ],
+    crime: [
+      "Assassinato", "Roubo", "Fraude", "Sequestro", "Tráfico",
+      "Estelionato", "Homicídio", "Extorsão", "Assalto", "Corrupção",
+      "Furto", "Crime Cibernético", "Lavagem de Dinheiro", "Estupro", "Apropriação Indébita",
+      "Roubo de Identidade", "Suborno", "Terrorismo", "Violência Doméstica", "Falsificação"
+    ],
+    era: [
+      "Idade Média", "Renascimento", "Era Vitoriana", "Idade Moderna", "Era Contemporânea",
+      "Pré-história", "Idade Antiga", "Renascimento Cultural", "Revolução Industrial", "Era Napoleônica",
+      "Belle Époque", "Reforma Protestante", "Era dos Descobrimentos", "Guerras Mundiais", "Guerra Fria",
+      "Era dos Vikings", "Idade do Bronze", "Era dos Piratas", "Revolução Francesa", "Idade do Ferro"
+    ],
+    festival: [
+      "Carnaval", "Oktoberfest", "Coachella", "Glastonbury", "Tomorrowland",
+      "Rock in Rio", "Lollapalooza", "Ultra Music Festival", "Festival de Cannes", "Festival de Sundance",
+      "Festival de Edimburgo", "Festival de Veneza", "Festival de Berlim", "Festival de Toronto", "Festival de Woodstock",
+      "Festival de Cannes", "Festival de Glastonbury", "Festival de Roskilde", "Festival de Sziget", "Festival de Reading"
+    ],
+    instrumentoMusical: [
+      "Violão", "Flauta Transversal", "Bateria Eletrônica", "Violino", "Teclado",
+      "Gaita de Fole", "Sitar", "Trombone", "Ocarina", "Acordeão",
+      "Viola", "Harmônica", "Fagote", "Bandolim", "Didgeridoo",
+      "Banjo", "Esraj", "Kalimba", "Erhu", "Berimbau"
+    ],
+    elementoQuimico: [
+      "Oxigênio", "Hidrogênio", "Carbono", "Ferro", "Ouro",
+      "Cobre", "Prata", "Alumínio", "Chumbo", "Urânio",
+      "Cálcio", "Potássio", "Sódio", "Enxofre", "Mercúrio",
+      "Fósforo", "Platina", "Zinco", "Níquel", "Tungstênio"
+    ],
+    mitologia: [
+      "Zeus", "Thor", "Anúbis", "Apolo", "Medusa",
+      "Odin", "Afrodite", "Hércules", "Poseidon", "Athena",
+      "Krishna", "Rama", "Shiva", "Brahma", "Vishnu",
+      "Quetzalcoatl", "Tezcatlipoca", "Huitzilopochtli", "Tlaloc", "Coatlicue"
+    ],
+    jogoTabuleiro: [
+      "War", "Catan", "Xadrez", "Dominó", "Detetive",
+      "Jogo da Vida", "Banco Imobiliário", "Scrabble", "Risco", "Tabu",
+      "Puerto Rico", "Agricola", "Dominion", "Ticket to Ride", "Pandemic",
+      "Carcassonne", "Settlers of Catan", "Chess", "Checkers", "Backgammon"
+    ],
+    objetoAntigo: [
+      "Máquina de Escrever", "Gramofone", "Máquina Fotográfica Analógica", "Relógio de Bolso", "Abajur a Querosene",
+      "Telefone de Disco", "Bengala", "Câmera de Vídeo VHS", "Máquina de Costura Antiga", "Rádio de Tubo",
+      "Máquina de Lavar Manual", "Televisor Preto e Branco", "Telegrafo", "Lanterna a Querosene", "Ferro de Passar a Carvão",
+      "Vitrola", "Piano de Pared", "Carruagem", "Espremedor de Suco Manual", "Relógio de Sol"
+    ],
+  };
+  
